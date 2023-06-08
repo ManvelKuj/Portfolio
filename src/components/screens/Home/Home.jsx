7138d76d4568bcc84c5e41e9496eed6a85339bf8
ManvelKuj <frontendKujoyan@yandex.ru>
2023-06-08 11:37:58 +0400

    portfolio

---
+++ src/components/screens/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <section id="home" class="pfblock-image screen-height">
+    <div class="home-overlay"></div>
+    <div class="intro">
+      <div class="start">Привет, меня зовут Манвел Куджоян и я</div>
+      <h1>Front end разработчик</h1>
+      <div class="start">Я занимаюсь разработкой сайтов с 2020 года.</div>
+    </div>
+
+    <a href="#services">
+      <div class="scroll-down">
+        <span>
+          aa
+        </span>
+      </div>
+    </a>
+  </section>
+  )
+}
